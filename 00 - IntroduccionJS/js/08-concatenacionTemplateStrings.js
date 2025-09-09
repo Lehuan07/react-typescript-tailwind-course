8e5f1d2c1f18d7acc5f0eb2b1812360db48b8941
Nahuel C. <nahuelncejas@gmail.com>
2025-09-09 19:36:34 -0300

    Template string y concatenacion

---
+++ 00 - IntroduccionJS/js/08-concatenacionTemplateStrings.js
@@ -0,0 +1,12 @@
+
+//*Template string y concatenacion
+
+const producto = "Tablet 32''"
+const precio = 400;
+const marca = "Orange"
+
+console.log("El producto es " + producto) //!Esto es concatenar
+
+console.log("El producto es " + producto + " $" + precio + " marca " + marca)
+
+console.log(`El producto es ${producto} $${precio} marca ${marca}`) //!Esto es Template String
