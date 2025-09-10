096dca2bf9f637a50585354531f9aefb3cb1e9de
Nahuel C. <nahuelncejas@gmail.com>
2025-09-10 10:41:55 -0300

    Todos los for

--- 00 - IntroduccionJS/js/12-RecorriendoArraysConFor.js
+++ 00 - IntroduccionJS/js/12-RecorriendoArraysConFor.js
@@ -6,3 +6,28 @@ const tecnologias = [
 for(let i = 0; i < tecnologias.length; i++){
     console.log(tecnologias[i])
 }
+
+//For
+const tecnologias2 = [
+    'HTML','CSS','JavaScript','React.js','Angular','Node.js'
+]
+
+//forEach
+tecnologias2.forEach(function(tech) {
+    console.log("Recorriendo desde forEach " + tech)
+});
+
+//Map
+
+const arregloNuevo = tecnologias2.map(function(tech) {
+    return tech;
+});
+
+console.log(arregloNuevo)
+
+//!La diferencia entre forEach y Map, es que el segundo te crea un nuevo arreglo
+
+//for...of
+for(let tech of tecnologias){
+    console.log("Recorriendo desde el for...of: " + tech)
+}
