9231ea63ad97b3b46b18c638be3e2a48801e1a30
Nahuel C. <nahuelncejas@gmail.com>
2025-09-10 10:05:48 -0300

    Update 10-modificarArrays.js

--- 00 - IntroduccionJS/js/10-modificarArrays.js
+++ 00 - IntroduccionJS/js/10-modificarArrays.js
@@ -1,7 +1,39 @@
 const tecnologias = [
     'HTML','CSS','JavaScript','React.js','Angular','Node.js'
 ]
+//Agregar
+tecnologias.push('Nest.js')
 
-tecnologias.push = 'nest.js'
+const nuevoArreglo =[
+    'Nest.js', ...tecnologias
+]
 
 console.log(tecnologias)
+
+console.table(nuevoArreglo)
+
+//Eliminar
+tecnologias.shift();
+console.log(tecnologias)
+
+//Filtrar
+const tecnologiasDos = tecnologias.filter(function(tech){
+    if(tech == "CSS")
+    return tech
+})
+
+console.log(tecnologiasDos)
+
+//Modificar
+
+tecnologias[3] = 'Python'
+
+const tecnologiastres = tecnologias.map(function(tech){
+    if(tech == 'Nest.js'){
+        return 'Nest.js'
+    }else{
+        tech
+    }
+})
+
+console.log(tecnologiastres)
