9b316308a2ff477f745b136dafd7c17411e39060
Nahuel C. <nahuelncejas@gmail.com>
2025-09-10 10:12:13 -0300

    Destruucturing de arreglos

---
+++ 00 - IntroduccionJS/js/11-DestructuringDeArrays.js
@@ -0,0 +1,10 @@
+const tecnologias = [
+    'HTML','CSS','JavaScript','React.js','Angular','Node.js'
+]
+
+const react = tecnologias[3];
+
+//Destruucturing de arreglos
+const [, , , reactJs] = tecnologias;
+
+console.log(reactJs)
