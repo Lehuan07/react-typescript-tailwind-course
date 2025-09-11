53bd1073fa5f03258dd6e0bb4b6c9e30c85759fd
Nahuel C. <nahuelncejas@gmail.com>
2025-09-11 09:43:20 -0300

    Introducción a ECMAScript Modules

--- 00 - IntroduccionJS/js/16-ReturnEnFunciones.js
+++ 00 - IntroduccionJS/js/16-ReturnEnFunciones.js
@@ -1,12 +1,11 @@
 //funtion Expression
 
-const sumar = function(num1 = 0, num2 = 0){
+export const sumar = function(num1 = 0, num2 = 0){
     return num1+num2
 }
 
 const resultado = sumar(1000, 2)
 console.log(resultado)
-
 
 function restar(num1, num2){
     return num1-num2
@@ -15,6 +14,6 @@ function restar(num1, num2){
 const resultadoResta = restar(2,5)
 console.log(resultadoResta)
 
-dividir =(num1 = 0, num2 = 0) => return num1/num2 //Da error por que es en una sola linea
+// dividir =(num1 = 0, num2 = 0) => return num1/num2 //Da error por que es en una sola linea
 
 
---
+++ 00 - IntroduccionJS/js/24-IntroducciónaECMAScriptModules.js
@@ -0,0 +1,7 @@
+import {sumar} from "./16-ReturnEnFunciones.js"
+
+const resultado = sumar (20, 30)
+
+console.log(resultado)
+
+//En el html en el script se pone  type="module
