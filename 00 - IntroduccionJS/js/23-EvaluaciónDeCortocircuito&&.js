e4ee19de24d577b106e6006b7d3ab01323eb4f71
Nahuel C. <nahuelncejas@gmail.com>
2025-09-11 09:33:12 -0300

    Evaluacion de corto circuito

---
+++ 00 - IntroduccionJS/js/23-EvaluaciónDeCortocircuito&&.js
@@ -0,0 +1,14 @@
+
+//Evaluacion de corto circuito
+
+const auth = true
+
+if(40){
+    console.log("Truthy")
+} else{
+    console.log("Falsy")
+}
+
+const user = {}
+
+user && console.log("Usuario autenticado")
